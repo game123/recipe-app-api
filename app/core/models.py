20c79900a1ca4ec7be25e0c4d77e52fe0cf79c3a
Kevin Chan <kevinykchan@gmail.com>
2020-03-15 22:48:27 -0400

    Added working list tags endpoint.

--- app/core/models.py
+++ app/core/models.py
@@ -3,6 +3,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
     PermissionsMixin
 
+from django.conf import settings
+
 
 class UserManager(BaseUserManager):
 
@@ -37,24 +39,14 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     USERNAME_FIELD = 'email'
 
-# class UserManager(BaseUserManager):
-#
-#     def create_user(self, email, password=None, **extra_fields):
-#         """ Creates and saves a new user"""
-#         user = self.model(email=email, **extra_fields)
-#         user.set_password(password)
-#
-#         user.save(using=self._db)
-#
-#         return user
-#
-# class User(AbstractBaseUser, PermissionsMixin):
-#     """Custom user model that supports using email instead of username"""
-#     email = models.EmailField(max_length=255, unique=True)
-#     name = models.CharField(max_length=255)
-#     is_active = models.BooleanField(default=True)
-#     is_staff = models.BooleanField(default=False)
-#
-#     objects = UserManager()
-#
-#     USERNAME_FIELD = 'email'
+
+class Tag(models.Model):
+    """Tag to be used for a recipe"""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
